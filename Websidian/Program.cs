e945d04ff4f4f731bf7249837f99d74b8b90c502
Sebastian Pech <spech@spech.de>
2022-09-04 09:14:52 +0200

    Update deps, remove weather

--- Websidian/Program.cs
+++ Websidian/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<VaultService>();
 
 var app = builder.Build();
